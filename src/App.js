1cf14485b1258bf7cfe5116e68acf9e274e668b3
Thiện Thành Nguyễn <thanhoilathanh482@gmail.com>
2022-07-17 20:01:30 +0700

    Location done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import { Navigate } from 'react-router-dom'
 import Currently_day from './Currently_day/Currently_day'
 import Header from './Header/Header'
@@ -11,8 +11,46 @@ import {
 import Home from './pages/Home/index'
 import HourlyWeather from './pages/HourlyWeather'
 const App = () => {
+  // Get user's current position-------------------
+  const [currentLocation, setCurrentLocation] = useState({
+    city: '',
+    country: '',
+  })
+  useEffect(() => {
+    if (navigator.geolocation) { //check if geolocation is available
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+      console.log("Geolocation is not available");
+    }
+  }, [])
+  function onSuccess(position) {
+    let {latitude , longitude} = position.coords;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=45d7160a0b1b4642b8333b74383909ba`)
+    // convert latitude and longitude to address
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.results[0].components.city);
+      console.log(data.results[0].components.country);
+      setCurrentLocation({
+        city: data.results[0].components.city,
+        country: data.results[0].components.country,
+      })
+    })
+  }
+  function onError(error) {
+    if (error.code == 1) {
+      alert('You have just denied location request!')// user denied location request
+    }
+    else if (error.code == 2) {
+      alert('Location not available')
+    } else {
+      alert('Something went wrong')
+    }
+  }
+  // ----------------------------------------------------------------
+  
+  
   const [inforWeather, setInforWeather] = useState(null)
-
   return (
     <div>
         <Header inforWeather={inforWeather} // quân
